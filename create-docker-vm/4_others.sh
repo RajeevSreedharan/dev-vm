0eb5c0de1789111a3f4d7652e4fc1414d21d5637
Rajeev Sreedharan <RajeevSreedharan@users.noreply.github.com>
2020-02-01 23:35:28 +0800

    Create 4_others.sh
    Other installations

---
+++ create-docker-vm/4_others.sh
@@ -0,0 +1,10 @@
+# Install VSCode
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+yum check-update
+yum install code # Usage : code --user-data-dir /home/administrator/Documents/VSCode
+
+# Install Kubernetes
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
